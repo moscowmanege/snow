7c9113e861a28f1407d7763f078d8a3136b43b1b
ponyspy <desade4me@gmail.com>
2015-12-23 13:57:45 +0300

    init

--- js/main.js
+++ js/main.js
@@ -5,27 +5,27 @@ $(document).ready(function() {
 	var parent_height = parent.height();
 
 	parent.on('scroll', function(event) {
-	    var s = $(this).scrollTop(),
-	        f = $('>:first', parent),l = $('>:last', parent);
-	    if(s > max) {f.appendTo(parent); parent.scrollTop(s - f.height())}
-	    if(s < 5) {l.prependTo(parent);parent.scrollTop(s + l.height()) }
-	}).scrollTop(5);
-
-	parent.on('scroll', function(event) {
-
-		$('.item').each(function() {
 			var $this = $(this);
-			var offset = $this.offset().top * 100 / parent_height * 1; // 20 or 1
-			$this.css('background-position', '50% ' + offset + '%');
-		});
-
-	});
+	    var s = $this.scrollTop(),
+	        f = $('>:first', $this),l = $('>:last', $this);
+	    if(s > max) {f.appendTo($this); $this.scrollTop(s - f.height())}
+	    if(s < 5) {l.prependTo($this); $this.scrollTop(s + l.height()) }
+	}).scrollTop(5);
 
 	parent.on('mouseover', function() {
 		var self = this;
+
+		parent.off('scroll.tiles').filter(self).on('scroll.tiles', function(event) {
+			$('.item').each(function() {
+				var $this = $(this);
+				var offset = $this.offset().top * 100 / parent_height * 1; // 20 or 1
+				$this.css('background-position', '50% ' + offset + '%');
+			});
+		});
+
 		parent.off('scroll.columns').filter(self).on('scroll.columns', function(event) {
 			position = $(this).scrollTop();
 			parent.not(self).scrollTop(position);
 		});
-	});
+	}).eq(0).trigger('mouseover');
 });
