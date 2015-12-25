da9ff048f8474596de355898c8e953cdbc5e350d
ponyspy <desade4me@gmail.com>
2015-12-25 22:28:02 +0300

    letters

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-	var parent = $('.column');
-	var parent_height = parent.height();
+	var parent_height = $('.container').height();
 	var $items = $('.item');
 
 	function repeatOften() {
