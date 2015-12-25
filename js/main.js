9f9e2adaa58d05aa337af632d05636399eea4c0e
ponyspy <desade4me@gmail.com>
2015-12-25 22:07:59 +0300

    clear

--- js/main.js
+++ js/main.js
@@ -11,20 +11,3 @@ $(document).ready(function() {
 	}
 	requestAnimationFrame(repeatOften);
 });
-
-
-// $(document).ready(function() {
-// 	var parent = $('.column');
-// 	var parent_height = parent.height();
-// 	var $items = $('.item');
-
-// 	function repeatOften() {
-// 		$items.each(function() {
-// 			var $this = $(this);
-// 			var offset = $this.offset().top * 100 / parent_height * 1; // 20 or 1
-// 			$this.css('background-position', '50% ' + offset + '%');
-// 		});
-// 	  requestAnimationFrame(repeatOften);
-// 	}
-// 	requestAnimationFrame(repeatOften);
-// });
